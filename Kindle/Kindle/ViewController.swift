6a706f7a6eae830c84cec9e7297ff451491b7a89
Shreyak Godala <shreyakgodala2909@gmail.com>
2019-09-24 20:12:50 +0530

    initial commit

---
+++ Kindle/Kindle/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Kindle
+//
+//  Created by Shreyak Godala on 24/09/19.
+//  Copyright © 2019 Shreyak Godala. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
